72c7fe0d133c29136ae958ebad340e35a5cc4538
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-30 21:49:29 +0800

    Add files via upload

---
+++ XYZ.ts
@@ -0,0 +1,44 @@
+import "Matrix.ts"
+declare module X{}
+declare module Y{}
+declare module Z{}
+function X(){
+    [`matrix0`][`A`][`B`][`C`][`D`] ,[`d`][`c`][`b`][`a`][`matrix0`];
+    [`H`][`matrix1`][`G`][`F`][`E`] ,[`e`][`f`][`g`][`matrix1`][`h`];
+    [`L`][`K`]['matrix2'][`J`][`I`] ,[`i`][`j`][`matrix2`][`k`][`l`];
+    [`P`][`O`][`N`][`matrix3`][`M`] ,[`m`][`matrix3`][`n`][`o`][`p`];
+    [`T`][`S`][`R`][`Q`][`matrix4`] ,[`matrix4`][`q`][`r`][`s`][`t`];
+
+    [`w1`][`v`][`u`][`t5`] [`matrix5`]  ,[`matrix5`][`t10`][`u`][`v`][`w2`];
+    [`t4`][`t3`][`t2`][`matrix6`][`t1`] ,['t6']['matrix6']['t7']['t8']['t9'];
+    [`T3`]['Z3']['matrix7'][`Y3`][`X3`] ,[`X6`][`Y6`]['matrix7'][`Z6`][`T6`];
+    [`T2`][`matrix8`][`Z2`][`Y2`][`X2`] ,[`X5`][`Y5`][`Z5`]['matrix8'][`T5`];
+    [`matrix9`][`T1`][`Z1`][`Y1`][`X1`] ,[`X4`][`Y4`][`Z4`][`T4`][`matrix9`]
+}
+function Y(){  
+    [`matrix0`][`A`][`B`][`C`][`D`]    ,[`d`][`c`][`b`][`a`][`matrix0`];
+    [`H`][`matrix1`][`G`][`F`][`E`]    ,[`e`][`f`][`g`][`matrix1`][`h`];
+    [`L`][`K`]['matrix2'][`J`][`I`]    ,[`i`][`j`][`matrix2`][`k`][`l`];
+    [`P`][`O`][`N`][`matrix3`][`M`]    ,[`m`][`matrix3`][`n`][`o`][`p`];
+    [`T`][`S`][`R`][`Q`][`matrix4`]    ,[`matrix4`][`q`][`r`][`s`][`t`];
+
+    [`w1`][`v`][`u`][`t5`] [`matrix5`]  ,[`matrix5`][`t10`][`u`][`v`][`w2`];
+    [`t4`][`t3`][`t2`][`t1`][`matrix6`] ,['matrix6'][`t6`]['t7']['t8']['t9'];
+    [`T3`]['Z3'][`Y3`][`X3`]['matrix7'] ,['matrix7'][`X6`][`Y6`][`Z6`][`T6`];
+    [`T2`][`Z2`][`Y2`][`X2`][`matrix8`] ,[`matrix8`][`X5`][`Z5`][`Y5`][`T5`];
+    [`T1`][`Z1`][`Y1`][`X1`][`matrix9`] ,[`matrix9`][`X4`][`Y4`][`Z4`][`T4`];
+}
+function Z(){
+     [`matrix0`][`matrix1`][`matrix2`][`matrix3`][`matrix4`][`matrix5`][`matrix6`][`matrix7`][`matrix8`][`matrix9`],
+     [`A`]    [`B`]      [`C`]    [`D`]      [`E`]      [`F`]    [`G`]    [`H`]      [`matrix8`]        [`I`],
+     [`J`]    [`K`]      [`L`]    [`M`]      [`N`]      [`O`]    [`P`]    [`matrix7`][`Q`]              [`R`],
+     [`S`]    [`T`]      [`U`]    [`V`]      [`W`]      [`X1`]   [`matrix6`][`Y1`]     [`Z1`]           [`T1`],
+     [`X2`]   [`Y2`]     [`Z2`]   [`T2`]     [`X3`]     [`t0`]   [`Y3`]   [`Z3`]     [`T3`]        [`X4`],
+     [`t1`]   [`t2`]     [`t3`]   [`t4`]     [`t5`]     [`t10`]  [`t9`]   [`t8`]     [`t7`]     [`t6`],
+    
+     [`X6`]    [`T6`]      [`Z6`]    [`Y6`]  [`matrix4`][`T5`]      [`X5`]    [`Y5`]    [`Z5`]     [`T4`],
+     [`Z4`]    [`Y4`]      [`X4`]    [`matrix3`][`w`]   [`v`]       [`u`]    [`t`]      [`s`]      [`r`],
+     [`q`]    [`p`]      [`matrix2`]         [`o`]      [`n`]       [`m`]    [`l`]      [`k`]      [`j`],
+     [`i`]   [`matrix1`]  [`h`]   [`g`]      [`f`]      [`e`]       [`d`]    [`c`]      [`b`]      [`a`],
+     [`matrix0`][`matrix1`][`matrix2`][`matrix3`][`matrix4`][`matrix5`][`matrix6`][`matrix7`][`matrix8`][`matrix9`]
+}
